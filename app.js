c4b7017038b887daba5efa8fc582c613750942f5
Storm Søndergaard <stormsoendergaard@gmail.com>
2022-01-22 17:22:21 +0100

    Progress

--- app.js
+++ app.js
@@ -40,12 +40,30 @@ function SaveUserData() {
 LoadUserData();
 
 
+// TODO: Make come up with propper architecture
+
 app.get('/', (req, res) => {
   res.render('index')
 })
 
 app.get('/nutrition', (req, res) => {
   res.render('nutrition', {nutrition: userData.nutrition})
+})
+
+// TODO: Rename
+app.patch('/nutrition/addsourcetoreport/:id', (req, res) => {
+  userData.nutrition.reports[0].sources.push(req.params.id)
+  userData.nutrition.reports[0].calories += parseInt(userData.nutrition.sources[req.params.id].calories)
+  SaveUserData();
+  res.redirect('/nutrition')
+})
+
+// TODO: Rename
+app.patch('/nutrition/removesourcefromreport/:id', (req, res) => {
+  userData.nutrition.reports[0].calories -= parseInt(userData.nutrition.sources[userData.nutrition.reports[0].sources[req.params.id]].calories)
+  userData.nutrition.reports[0].sources.splice(req.params.id, 1)
+  SaveUserData();
+  res.redirect('/nutrition')
 })
 
 app.post('/nutrition/addsource', function (req, res) {
